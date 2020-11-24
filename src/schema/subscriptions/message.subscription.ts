172c6261579d2d07e97e0e174fcb015ee102a1d2
Hien <hienlh1298@gmail.com>
2020-11-24 15:08:30 +0000

    refactor: update message subscription

--- src/schema/subscriptions/message.subscription.ts
+++ src/schema/subscriptions/message.subscription.ts
@@ -1,8 +1,12 @@
-import { intArg, subscriptionField } from "@nexus/schema";
+import { idArg, intArg, stringArg, subscriptionField } from "@nexus/schema";
+import { Conversation, Message } from "@prisma/client";
 import { withFilter } from "apollo-server-express";
 import SubscriptionKey from "../../constants/subscriptionKey";
-import { pubsub } from "../../server";
+import { Context } from "../../context";
+import { prisma, pubsub } from "../../server";
 import { AuthService } from "../../service";
+import { MessageService } from "../../service/message.service";
+import { withCancel } from "../../utils/helper";
 
 export const MessageSubscription = subscriptionField(
     "newMessage",
@@ -10,17 +14,66 @@ export const MessageSubscription = subscriptionField(
         type: 'Message',
         description: 'Nếu không truyền vào người gửi thì sẽ nhận tất cả tin nhắn từ bất kì bạn bè nào gửi tới',
         args: {
-            senderId: intArg({ nullable: true })
+            conversationId: idArg(),
+            senderId: idArg()
         },
-        subscribe: withFilter(
-            (_root, _args, ctx, _info) => {
+        subscribe: (root, args, ctx, info) => withCancel((withFilter(
+            (_root, _args, ctx: Context, _info) => {
                 AuthService.authenticate(ctx);
+                MessageService.updateInChatStatus(ctx, {
+                    conversationId: args.conversationId,
+                    otherUserId: args.senderId
+                }, true);
                 return pubsub.asyncIterator(SubscriptionKey.newMessage);
             },
-            (root, args, ctx, _info) => {
-                return args.senderId ? args.senderId === root.senderId : root.receiverId === ctx.user.id
+            async (root: Message, args, ctx, _info) => {
+                return (args.conversationId
+                    ? root.conversationId === args.conversationId
+                    : (args.senderId ? args.senderId === root.senderId || ctx.user.id === root.senderId : true))
+                    && !!await prisma.conversationMember.findOne({
+                        where: {
+                            conversationId_userId: {
+                                conversationId: root.conversationId,
+                                userId: ctx.user.id
+                            }
+                        }
+                    })
             }
-        ),
+        ))(root, args, ctx, info), () => {
+            MessageService.updateInChatStatus(ctx, {
+                conversationId: args.conversationId,
+                otherUserId: args.senderId
+            }, false);
+        }),
+        resolve: (payload) => {
+            return payload;
+        },
+    }
+)
+
+export const ConversationChangeSubscription = subscriptionField(
+    "conversationChange",
+    {
+        type: 'Conversation',
+        subscribe: (root, args, ctx, info) => withCancel((withFilter(
+            (_root, _args, ctx, _info) => {
+                AuthService.authenticate(ctx);
+                // MessageService.updateInChatStatus(ctx, {}, true);
+                return pubsub.asyncIterator(SubscriptionKey.conversationChange);
+            },
+            async (root: Conversation, _args, ctx, _info) => {
+                return !!await prisma.conversationMember.findOne({
+                    where: {
+                        conversationId_userId: {
+                            conversationId: root.id,
+                            userId: ctx.user.id
+                        }
+                    }
+                })
+            }
+        ))(root, args, ctx, info), () => {
+            // MessageService.updateInChatStatus(ctx, {}, false);
+        }),
         resolve: (payload) => {
             return payload;
         },
