37a9ac687a6dee1dad3587067e2c026dccce01c0
Hien <hienlh1298@gmail.com>
2020-12-10 16:35:47 +0000

    fix: login social without register

--- src/schema/mutations/auth.mutation.ts
+++ src/schema/mutations/auth.mutation.ts
@@ -2,6 +2,7 @@ import { arg, enumType, mutationField, objectType, stringArg } from "@nexus/sche
 import { Config, ConstConfig } from "../../config";
 import { JwtAuthPayload } from "../../model/jwtPayload";
 import { JwtRegisterPayload } from "../../model/registerPayload";
+import { DefaultRole } from "../../model/role";
 import { prisma } from "../../server";
 import { AuthService, UserService } from "../../service";
 import { SocialNetworkService } from "../../service/socialNetwork.service";
@@ -21,20 +22,7 @@ export const LoginMutation = mutationField('login', {
 })
 
 export const LoginSocialNetwork = mutationField('loginSocialNetwork', {
-    type: objectType({
-        name: 'LoginSocialOutput',
-        definition(t) {
-            t.field('tokenType', {
-                type: enumType({
-                    name: 'LoginSocialTokenType',
-                    members: ['register', 'login']
-                })
-            })
-            t.string('token', { nullable: false })
-            t.field('data', { type: 'SocialProvider', nullable: true })
-            t.field('info', { type: 'User', nullable: true })
-        }
-    }),
+    type: LoginOutputType,
     args: {
         type: arg({ type: 'SocialProviderEnumType', nullable: false }),
         accessToken: stringArg({ nullable: false }),
@@ -42,25 +30,30 @@ export const LoginSocialNetwork = mutationField('loginSocialNetwork', {
     },
     resolve: async (_root, args, _ctx, _info) => {
         const socialData = await SocialNetworkService.login(args.type, args.accessToken);
-        const socialProvider = await prisma.socialProvider.findOne({
+        let socialProvider = await prisma.socialProvider.findOne({
             where:
                 { id_type: { type: socialData.type, id: socialData.id } },
-            include: { user: true }
         })
 
-        if (socialProvider) {
-            const loginData = await AuthService.login(args.type, socialData.id, args.deviceId);
-            return {
-                tokenType: 'login',
-                ...loginData
-            }
-        } else {
-            return {
-                tokenType: 'register',
-                token: AuthService.sign<JwtRegisterPayload>({ ...socialData }, Config.registerExpireTime.value),
-                data: socialData
-            }
+        if (!socialProvider) {
+            socialProvider = await prisma.socialProvider.create({
+                data: {
+                    ...socialData,
+                    user: {
+                        create: {
+                            nickName: socialData.name ?? socialData.id,
+                            role: { connect: { id: DefaultRole.normal.id } },
+                            ...socialData.avatar ? { avatar: { create: { type: 'image', url: socialData.avatar } } } : {},
+                            ...socialData.birthday ? { birthday: socialData.birthday } : {},
+                            ...socialData.phoneNumber ? { phoneNumber: socialData.phoneNumber } : {},
+                            ...socialData.gender ? { gender: socialData.gender } : {},
+                        }
+                    }
+                }
+            })
         }
+
+        return await AuthService.login(args.type, socialProvider.id, args.deviceId);
     }
 })
 
